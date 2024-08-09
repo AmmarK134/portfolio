24a7251aa398d701dac914be853eba4c8fff8b42
Ammar Kashif <ammar.kashif@torontomu.ca>
2024-08-08 23:11:52 -0400

    Updated layout

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -6,10 +6,18 @@ const Home = () => {
   return (
     <section id="home" className="home">
       <div className="home-content">
-        <img src={ammar} alt="Profile" className="home-image" />
+        <img src={ammar} alt="Ammar" className="ammarPic" />
         <div className="home-text">
-          <h2>Welcome to My Portfolio</h2>
-          <p>Here you can find information about me, my projects, and how to contact me.</p>
+          <h2>Hi</h2>
+          <p>I’m Ammar Kashif, a dedicated Computer Science student at 
+            Toronto Metropolitan University with a passion for programming 
+            and game development. With over four years of experience in Java
+            and a solid foundation in Python, I am currently expanding my 
+            skills in web development with HTML, CSS, and JavaScript. 
+            My journey includes freelance software development, where 
+            I’ve created custom games and enhanced existing applications, 
+            as well as experience as a Computer Science tutor, fostering 
+            a positive learning environment for students.</p>
         </div>
       </div>
     </section>
